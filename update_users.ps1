48795ad5a3769e56cc4e38abe83bbd9e2a27f55b
MissingNoShiny <larcinvincent@gmail.com>
2020-11-18 12:59:29 +0100

    Type a message to commit

--- update_users.ps1
+++ update_users.ps1
@@ -1,11 +1,15 @@
 $Suffix = (Get-ADDomain).Forest
 
-Get-ADUser -Filter '-Not [string]::IsNullOrEmpty($_.UserPrincipalName)' | ForEach-Object {
-    $NewUPN = "$($_.GivenName).$($_.Surname)".ToLower() -replace "[ -]", ""
+Get-ADUser -Filter * | ForEach-Object {
+    If (-Not [string]::IsNullOrEmpty($_.UserPrincipalName)) { 
+        $NewUPN = "$($_.GivenName).$($_.Surname)".ToLower() -replace "[ -]", ""
 
-    Set-ADUser -Identity $_.UserPrincipalName `
-        -UserPrincipalName "$NewUPN@$Suffix" `
-        -ChangePasswordAtLogon $False `
-        -Name "$($_.GivenName) $($_.Surname)"
+        Set-ADUser -Identity $_.UserPrincipalName `
+            -UserPrincipalName "$NewUPN@$Suffix" `
+            -ChangePasswordAtLogon $False `
+            -DisplayName "$($_.GivenName) $($_.Surname)"
+    }
+
+    Rename-ADObject -Identity $_.DistinguishedName -NewName "$($_.GivenName) $($_.Surname)"
 }
 
