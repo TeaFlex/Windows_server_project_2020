70c81433ee5e0e8b23a15cd03fe0fc3761cb3441
MissingNoShiny <larcinvincent@gmail.com>
2020-11-18 09:01:25 +0100

    Suppression OU récursive

--- delete_ou.ps1
+++ delete_ou.ps1
@@ -1,5 +1,5 @@
 Get-ADOrganizationalUnit -Filter 'Name -NotLike "Domain Controllers"' | ForEach-Object {
-    Remove-ADOrganizationalUnit -Identity $_.DistinguishedName -Confirm:$False
+    Remove-ADOrganizationalUnit -Identity $_.DistinguishedName -Recursive -Confirm:$False
 }
 
 Get-ADGroup -Filter '(Name -Like "GG_*") -Or (Name -Like "GL_*")' | ForEach-Object {
