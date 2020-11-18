34b6bc881e9fd09e5d60d055ed7488ab9d6de6c4
MissingNoShiny <larcinvincent@gmail.com>
2020-11-18 08:56:30 +0100

    Script de nettoyage

---
+++ delete_ou.ps1
@@ -0,0 +1,7 @@
+Get-ADOrganizationalUnit -Filter 'Name -NotLike "Domain Controllers"' | ForEach-Object {
+    Remove-ADOrganizationalUnit -Identity $_.DistinguishedName -Confirm:$False
+}
+
+Get-ADGroup -Filter '(Name -Like "GG_*") -Or (Name -Like "GL_*")' | ForEach-Object {
+    Remove-ADGroup -Identity $_.DistinguishedName -Confirm:$False
+}
