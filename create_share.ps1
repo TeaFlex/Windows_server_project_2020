14dd55334080a4dca83e42185b200ea0218d2a8a
Vincent Larcin <larcinvincent@gmail.com>
2020-11-19 06:43:21 +0100

    Non récursif

--- create_share.ps1
+++ create_share.ps1
@@ -1,3 +1,4 @@
+<#
 Function Add-ShareDirectory($OUPath, $DirectoryPath) {
     Get-ADOrganizationalUnit -Filter 'Name -NotLike "Domain Controllers"' -SearchBase $Path -SearchScope 1 | ForEach-Object {
         $Name = $_.Name
@@ -5,7 +6,28 @@ Function Add-ShareDirectory($OUPath, $DirectoryPath) {
         Add-ShareDirectory("OU=$Name,$OUPath", "$DirectoryPath\$Name")
     }
 }
+#>
 
 
 New-Item -Path "C:\" -Name "Share" -ItemType "Directory"
-Add-ShareDirectory (Get-ADDomain).DistinguishedName "C:\Share"
+#Add-ShareDirectory (Get-ADDomain).DistinguishedName "C:\Share"
+
+$DomainPath = (Get-ADDomain).DistinguishedName
+
+Get-ADOrganizationalUnit -Filter 'Name -NotLike "(Domain Controllers)|(Groupes)' -SearchBase $DomainPath -SearchScope 1 | ForEach-Object {
+    $Name = $_.Name
+    New-Item -Path "C:\Share" -Name $Name -ItemType "Directory"
+    $DirPath = "C:\Share\$Name"
+
+    #$Rule = New-Object System.Security.AccessControl.FileSystemAccessRule()
+    $InnerOUs = Get-ADOrganizationalUnit -Filter * -SearchBase "OU=$($_.Name),$DomainPath"  -SearchScope 1
+
+    $InnerOUs | ForEach-Object {
+        New-Item -Path $DirPath -Name $_.Name -ItemType "Directory"
+
+        
+    }
+
+}
+
+
