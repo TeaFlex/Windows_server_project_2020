54340712833b0e928782a425fabde08ec66cee29
VitaliLuca <vitalilucaster@gmail.com>
2020-11-19 10:51:23 +0100

    Ajout de logs

--- create_share.ps1
+++ create_share.ps1
@@ -1,3 +1,10 @@
+#Ecrit des fichiers de log
+function Write-LogFile($Content,$Type){
+    if ($Type -eq "Daily"){
+        Write-Output "$(Get-Date -Format "HH:mm:ss")`t$Content" | Tee-Object -Append "$(Get-Date -Format "ddMMyy").log"
+    }
+    Write-Output "$(Get-Date -Format "HH:mm:ss")`t$Content" >> "log_CreateShare.log"
+}
 Function Add-FolderPermission($GroupSID, $DirPath, $PermissionType, $PermissionValue) {
     $Acl = Get-Acl $DirPath
     $Perm = $GroupSID, $PermissionType, "ContainerInherit,ObjectInherit", "None", $PermissionValue
@@ -5,6 +12,9 @@ Function Add-FolderPermission($GroupSID, $DirPath, $PermissionType, $PermissionV
     $Acl.SetAccessRule($Rule)
     $Acl | Set-Acl -Path $DirPath
 }
+
+#Ecrit dans le fichier de log journalier le début de l'exécution du script
+Write-LogFile "Debut de l'execution du script $($MyInvocation.MyCommand.Name)" "Daily"
 
 $GroupPrefix = (Get-ADDomain).NetBIOSName
 
@@ -16,6 +26,7 @@ $DomainPath = (Get-ADDomain).DistinguishedName
 Get-ADOrganizationalUnit -Filter 'Name -NotLike "(Domain Controllers)|(Groupes)"' -SearchBase $DomainPath -SearchScope 1 | ForEach-Object {
     $Name = $_.Name
     New-Item -Path "C:\Share" -Name $Name -ItemType "Directory"
+    Write-LogFile "Creation du dossier $Name"
     $DirPath = "C:\Share\$Name"
 
     Add-FolderPermission (Get-ADGroup -Filter "Name -Eq `"GL_$Name`_R`"").SID $DirPath "Read" "Allow"
@@ -29,8 +40,11 @@ Get-ADOrganizationalUnit -Filter 'Name -NotLike "(Domain Controllers)|(Groupes)"
         $InnerName = $_.Name
 
         New-Item -Path $DirPath -Name $InnerName -ItemType "Directory"
+        Write-LogFile "Creation du dossier $Name/$InnerName"
         Add-FolderPermission (Get-ADGroup -Filter "Name -Eq `"GL_$InnerName`_RW`"").SID "$DirPath\$Name" "Read,Modify" "Allow"
         
     }
-
 }
+
+#Ecrit dans le fichier de log journalier la fin de l'exécution du script
+Write-LogFile "Fin de l'execution du script $($MyInvocation.MyCommand.Name)" "Daily"
